302f68b1123ced67b304a4c1910b3252cdde7939
ELCT ND SACCOS <info@elctndsaccos.org>
2026-04-24 16:00:25 +0300

    Update contact information in header and footer
    - Removed old email info@elctndsaccos.co.tz and phone +255 27 250 1234
    - Removed Online Banking and Branch Locator links from top bar
    - Updated footer with KNCU Building address: Kibo Road, P.O.Box 7779 - Moshi
    - Changed email to info@elctndsaccos.org in footer
    - Maintained clean contact section structure

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -24,18 +24,9 @@
                             <path d="M2.003 5.884L10 9.882l7.997-3.998A2 2 0 0016 4H4a2 2 0 00-1.997 1.884z"/>
                             <path d="M18 8.118l-8 4-8-4V14a2 2 0 002 2h12a2 2 0 002-2V8.118z"/>
                         </svg>
-                        info@elctndsaccos.co.tz
-                    </span>
-                    <span class="flex items-center">
-                        <svg class="w-4 h-4 mr-1" fill="currentColor" viewBox="0 0 20 20">
-                            <path d="M2 3a1 1 0 011-1h2.153a1 1 0 01.986.836l.74 4.435a1 1 0 01-.54 1.06l-1.548.773a11.037 11.037 0 006.105 6.105l.774-1.548a1 1 0 011.059-.54l4.435.74a1 1 0 01.836.986V17a1 1 0 01-1 1h-2C7.82 18 2 12.18 2 5V3z"/>
-                        </svg>
-                        +255 27 250 1234
+                        info@elctndsaccos.org
                     </span>
                 </div>
-                <div class="flex items-center space-x-4">
-                    <a href="#" class="hover:text-primary-200 transition-colors">Online Banking</a>
-                    <a href="#" class="hover:text-primary-200 transition-colors">Branch Locator</a>
                     <div class="flex items-center space-x-2">
                         <a href="#" class="hover:text-primary-200 transition-colors">
                             <svg class="w-5 h-5" fill="currentColor" viewBox="0 0 24 24">
